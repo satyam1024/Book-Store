d474f01ef6595474d25ae86881a5ce53b709fca5
satyam1024 <SATYAMKATHIAT.220111867@gehu.ac.in>
2024-09-04 13:02:39 +0530

    completed create book

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Spinner() {
   return (
-    <div className='animate-ping w-16 h-16 m-8 rounded-full blue-sky-600'>Spinner</div>
+    <div className='animate-ping w-16 h-16 m-8 rounded-full blue-sky-600'></div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     const [loading,setLoading]=useState(false);
     useEffect(()=>{
         setLoading(true);
-        axios.get('https://localhost:5555/books').then((res)=>{
+        axios.get('http://localhost:5555/books').then((res)=>{
             setBooks(res.data.data);
             setLoading(false);
         }).catch((error)=>{
@@ -21,20 +21,20 @@ export default function Home() {
 
     },[]);
   return (
-    <div classNmae ='p-4'>
+    <div className ='p-4'>
         <div className='flex justify-between items center'>
             <h1 className='text-3x1 my-8'>
                 Book list
             </h1>
             <Link to ='/books/create'>
-            {/* <MdOutlineAddBox className='text-sky-800 text-4x1'/> */}
+            <MdOutlineAddBox className='text-sky-800 text-4x1'/>
             </Link>
         </div>
         {loading?(
             <Spinner/>
         ):(
             <table className='w-full border-seprate border-spacing-2'>
-                <thread>
+                <tbody>
                     <tr>
                         <th className='border border-slate-600 rounded-md'>No</th>
                         <th className='border border-slate-600 rounded-md'>Title</th>
@@ -42,7 +42,7 @@ export default function Home() {
                         <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
                         <th className='border border-slate-600 rounded-md '>Operations</th>
                     </tr>
-                </thread>
+                </tbody>
                 <tbody>
                     {books.map((book,index)=>(
                         <tr key ={book._id} className='h-8'>
@@ -54,6 +54,12 @@ export default function Home() {
                                 <div className='flex justify-center gap-x-4'>
                                     <Link to={'/books/details/${book._id}'}>
                                         <BsInfoCircle className='text-2x1 text-green-800'></BsInfoCircle>
+                                    </Link>
+                                    <Link to={'/books/details/${book._id}'}>
+                                        <AiOutlineEdit className='text-2x1 text-yellow-600'></AiOutlineEdit>
+                                    </Link>
+                                    <Link to={'/books/details/${book._id}'}>
+                                        <MdOutlineDelete className='text-2x1 text-red-600'></MdOutlineDelete>
                                     </Link>
                                 </div>
                             </td>
